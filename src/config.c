05171a255afb21b6aea118870253f47924809c2f
xtreme8000 <xtreme8000@users.noreply.github.com>
2022-02-07 19:24:07 +0100

    Add hidden show_player_arms setting
    unfinished, for those who wanted it

--- src/config.c
+++ src/config.c
@@ -87,6 +87,7 @@ void config_save() {
 	config_setf("client", "camera_fov", settings.camera_fov);
 	config_seti("client", "hold_down_sights", settings.hold_down_sights);
 	config_seti("client", "chat_shadow", settings.chat_shadow);
+	config_seti("client", "show_player_arms", settings.player_arms);
 
 	for(int k = 0; k < list_size(&config_keys); k++) {
 		struct config_key_pair* e = list_get(&config_keys, k);
@@ -159,6 +160,8 @@ static int config_read_key(void* user, const char* section, const char* name, co
 			settings.hold_down_sights = atoi(value);
 		} else if(!strcmp(name, "chat_shadow")) {
 			settings.chat_shadow = atoi(value);
+		} else if(!strcmp(name, "show_player_arms")) {
+			settings.player_arms = atoi(value);
 		}
 	}
 	if(!strcmp(section, "controls")) {
